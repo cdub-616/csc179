96bdf0e67a1ca511c06b9292b207304fd01b5875
cdub-616 <113482799+cdub-616@users.noreply.github.com>
2023-07-04 11:30:08 -0700

    more add testing, including performance testing

--- addItem.test.js
+++ addItem.test.js
@@ -93,18 +93,312 @@ describe("my test suite", () => {
         .toBeUndefined;
     });
 
-    it('add item with illegal timestampAdded should fail', async () => {
-        await expect(add('item', '', '', true, '20230619T08:42:00.000Z'))
+    it('add item with illegal timestampAdded no dashes should fail', async () =>
+        { await expect(add('item', '', '', true, '20230619T08:42:00.000Z'))
         .toBeUndefined;
     })
 
-    it('add item with illegal timestampAdded should fail', async () => {
+    it('add item with illegal timestampAdded year should fail', async () => {
         await expect(add('item', '', '', true, '23-06-19T08:42:00.000Z'))
         .toBeUndefined;
     })
 
-    it('add item with illegal timestampAdded should fail', async () => {
-        await expect(add('item', '', '', true, '2023-19T08:42:00.000Z'))
+    it('add item with illegal timestampAdded no month should fail', async () => 
+        { await expect(add('item', '', '', true, '2023-19T08:42:00.000Z'))
         .toBeUndefined;
     })*/
+
+    //for July 8
+    it('add item timestampAdded no year should fail', async () => {
+        await expect(add('item', '', '', true, '07-04T10:20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded alpha year should fail', async () => { 
+        await expect(add('item', '', '', true, 'abcd-07-04T10:20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded alpha month should fail', async () => { 
+        await expect(add('item', '', '', true, '2023-ab-04T10:20:00.000Z'))
+        .toBeUndefined;
+    });
+    
+    it('add item timestampAdded short month should fail', async () => { 
+        await expect(add('item', '', '', true, '2023-7-04T10:20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded illegal month should fail', async () => {
+        await expect(add('item', '', '', true, '2023-20-04T10:20:00.00Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded no day should fail', async () => {
+        await expect(add('item', '', '', true, '23-07T10:20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded alpha day should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-abT10:20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded short day should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-4T10:20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded illegal day should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-63T10:20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded no hour should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded alpha hour should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04Tab:20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded short hour should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T1:20:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded illegal hour should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T32:20:000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded no minute should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T10:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded alpha minute should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T10:ab:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded short minute should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T10:2:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded illegal minute should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T10:62:00.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded no second should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T10:20.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded alpha second should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T10:20:ab.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded short second should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T10:20:7.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded illegal second should fail', async () => {
+        await expect(add('item', '', '', true, '23-07-04T10:20:70.000Z'))
+        .toBeUndefined;
+    });
+
+    it('add item timestampAdded alpha millisecond should fail', async () =>
+    { await expect(add('item', '', '', true, '23-07-04T10:20:00.abcZ'))
+        .toBeUndefined;
+    });
+
+    it('add item illegal timestampPurchased no dashes should fail', async () =>
+        { await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z', 
+        '20230619T08:42:00.000Z')).toBeUndefined;
+    })
+
+    it('add item illegal timestampPurchased year should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-06-19T08:42:00.000Z')).toBeUndefined;
+    })
+
+    it('add item illegal timestampPurchased no month should fail', async () => 
+        { await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '2023-19T08:42:00.000Z')).toBeUndefined;
+    })
+
+    it('add item timestampPurchased no year should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '07-04T10:20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased alpha year should fail', async () => { 
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        'abcd-07-04T10:20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased alpha month should fail', async () => { 
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '2023-ab-04T10:20:00.000Z')).toBeUndefined;
+    });
+    
+    it('add item timestampPurchased short month should fail', async () => { 
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '2023-7-04T10:20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased illegal month should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '2023-20-04T10:20:00.00Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased no day should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07T10:20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased alpha day should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-abT10:20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased short day should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-4T10:20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased illegal day should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-63T10:20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased no hour should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased alpha hour should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04Tab:20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased short hour should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T1:20:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased illegal hour should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T32:20:000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased no minute should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T10:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased alpha minute should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T10:ab:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased short minute should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T10:2:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased illegal minute should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T10:62:00.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased no second should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T10:20.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased alpha second should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T10:20:ab.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased short second should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T10:20:7.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased illegal second should fail', async () => {
+        await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T10:20:70.000Z')).toBeUndefined;
+    });
+
+    it('add item timestampPurchased alpha millisecond should fail', async () =>
+        { await expect(add('item', '', '', true, '2023-07-04T10:20:00.000Z',
+        '23-07-04T10:20:00.abcZ')).toBeUndefined;
+    });
+
+    //performance testing
+    for (var x = 0; x < 20; x++){
+        it('add item for performance test', async() => {
+            await expect(add('widget1', '100000', 'Sacramento', true,
+            '2023-07-04T10:14:00.000Z', '2023-07-04T10:14:00.000Z'))
+            .toBeDefined;
+        })
+    }
+
+    for (var x = 0; x < 30; x++){
+        it('add item for performance test', async() => {
+            await expect(add('widget2', '100000', 'Sacramento', true,
+            '2023-07-04T10:14:00.000Z', '2023-07-04T10:14:00.000Z'))
+            .toBeDefined;
+        })
+    }
+
+    for (var x = 0; x < 50; x++){
+        it('add item for performance test', async() => {
+            await expect(add('widget3', '100000', 'Sacramento', true,
+            '2023-07-04T10:14:00.000Z', '2023-07-04T10:14:00.000Z'))
+            .toBeDefined;
+        })
+    }
+
+    for (var x = 0; x < 100; x++){
+        it('add item for performance test', async() => {
+            await expect(add('widget4', '100000', 'Sacramento', true,
+            '2023-07-04T10:14:00.000Z', '2023-07-04T10:14:00.000Z'))
+            .toBeDefined;
+        })
+    }
+    
+    for (var x = 0; x < 250; x++){
+        it('add item for performance test', async() => {
+            await expect(add('widget5', '100000', 'Sacramento', true,
+            '2023-07-04T10:14:00.000Z', '2023-07-04T10:14:00.000Z'))
+            .toBeDefined;
+        })
+    }
+
+    for (var x = 0; x < 500; x++){
+        it('add item for performance test', async() => {
+            await expect(add('widget6', '100000', 'Sacramento', true,
+            '2023-07-04T10:14:00.000Z', '2023-07-04T10:14:00.000Z'))
+            .toBeDefined;
+        })
+    }
+
+    for (var x = 0; x < 1000; x++){ 
+        it('add item for performance test', async () => {
+            await expect(add('widget7', '1000000', 'Sacramento', true, 
+            '2023-07-04T10:14:00.000Z', '2023-07-04T10:14:00.000Z'))
+            .toBeDefined;
+        });
+    };
+
 });
